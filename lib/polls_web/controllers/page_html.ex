526f556abe1815c80836e1cd487b22aafa25cac1
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-07-18 15:52:26 +0530

    Removed unneeded bloat comments.

--- lib/polls_web/controllers/page_html.ex
+++ lib/polls_web/controllers/page_html.ex
@@ -1,9 +1,4 @@
 defmodule PollsWeb.PageHTML do
-  @moduledoc """
-  This module contains pages rendered by PageController.
-
-  See the `page_html` directory for all templates available.
-  """
   use PollsWeb, :html
 
   embed_templates "page_html/*"
