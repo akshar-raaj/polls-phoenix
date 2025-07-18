690888f2ac201cbf74a3e33ba5b3650d25c06099
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-07-18 21:22:07 +0530

    Added Ecto and PostgreX as dependency

--- mix.exs
+++ mix.exs
@@ -54,7 +54,9 @@ defmodule Polls.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, ">= 0.0.0"}  # for PostgreSQL
     ]
   end
 
